074cb06bea2f987c067239b90b5033d88121cef6
Rei Sikk <rei@hable.ee>
2025-11-03 19:22:37 +0200

    Refactored footer to match new Bento grid design

--- src/app/components/Footer/SiteFooter.tsx
+++ src/app/components/Footer/SiteFooter.tsx
@@ -8,13 +8,17 @@ function SiteFooter() {
     <footer className={`${styles.siteFooter} container`}>
       <div className={`${styles.siteFooter__main} grid`}>
         <div className={`${styles.siteFooter__cell} ${styles.siteFooter__cell_1} fp`}>
-          <h4 className={styles.siteFooter__title}>Run Clubs Estonia</h4>
+          <h4 className={styles.siteFooter__title}>
+            Run Clubs <br />
+            Estonia
+          </h4>
         </div>
         <div className={`${styles.siteFooter__row} fp`}>
           <div className={`${styles.siteFooter__cell} ${styles.siteFooter__cell_2} fp`}>
-            <span className='h4'>All rights reserved © {new Date().getFullYear()}</span>
+            <span className='h4'>All rights reserved.
+              <br />© {new Date().getFullYear()}</span>
           </div>
-          <div className={`${styles.siteFooter__cell} ${styles.siteFooter__cell_3} fp-col`}>
+          <div className={`${styles.siteFooter__cell} ${styles.siteFooter__cell_3} ${styles.hover_brightness} fp-col`}>
               <p className="h4">
                   This helps cover hosting & domain costs.<br />
                   Thank you 🙏
@@ -24,7 +28,7 @@ function SiteFooter() {
               target="_blank"
               href="https://www.buymeacoffee.com/tlnrunclubs"
               rel="noopener noreferrer"
-              data-umami-event="Clicked Buy Me A Coffee in Footer"
+              data-umami-event="Clicked Buy Us A Coffee in Footer"
             >
               <Image
                 unoptimized
@@ -32,26 +36,26 @@ function SiteFooter() {
                 width={32}
                 className="coffeeImage"
                 src="https://cdn.buymeacoffee.com/buttons/bmc-new-btn-logo.svg"
-                alt="Icon"
+                alt="Coffee icon"
               />
-              <span className="txt-body">Buy me a coffee</span>
+              <span className="txt-body">Buy us a coffee</span>
             </a>
           </div>
         </div>
       </div>
       <div className={`${styles.siteFooter__side} grid`}>
-        <div className={`${styles.siteFooter__cell} ${styles.siteFooter__cell_4} fp-col`}>
+        <div className={`${styles.siteFooter__cell} ${styles.siteFooter__cell_4} ${styles.hover_brightness} fp-col`}>
           <LucideSend size={96} strokeWidth={1} />
-          <a href="mailto:sikkrei@gmail.com" className='h4'>
+          <a href="mailto:sikkrei@gmail.com?subject=Feature request for Run Clubs Estonia" className='h4'>
             Have a feature request?<br />
-            Message me.
+            Message us.
           </a>
         </div>
-        <div className={`${styles.siteFooter__cell} ${styles.siteFooter__cell_5} fp`}>
+        <div className={`${styles.siteFooter__cell} ${styles.siteFooter__cell_5} ${styles.hover_brightness} fp`}>
           <a href='#page-top' className='h4'>Back to the top</a>
           <LucideArrowUp size={48} strokeWidth={1} />
         </div>
-        <div className={`${styles.siteFooter__cell} ${styles.siteFooter__cell_6}`}>
+        <div className={`${styles.siteFooter__cell} ${styles.siteFooter__cell_6} ${styles.hover_brightness}`}>
            <a 
             href="https://www.reihopsti.ee" 
             target="_blank" 
